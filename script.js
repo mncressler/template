0169c5125d7d37e423009ebaea3f397ffc39cdf5
Natalie Calhoun <nataliecalhoun@Natalies-MacBook-Air.local>
2023-01-12 10:19:02 -0800

    completed quote generator

---
+++ script.js
@@ -0,0 +1,66 @@
+const quoteContainer = document.getElementById('quote-container'); 
+const quoteText = document.getElementById('quote'); 
+const authorText = document.getElementById('author'); 
+const twitterBtn = document.getElementById('twitter'); 
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader'); 
+
+let apiQuotes = [];
+
+
+// show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// Show New Quote
+function newQuote() {
+    loading();
+    // Pick a random quote from API quotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    authorText.textContent = quote.author;
+
+    // Check quote length to determine styling
+    if (quote.text.length > 100) {
+        quoteText.classList.add('long-quote');}
+        else {
+            quoteText.classList.remove('long-quote');}
+
+    quoteText.textContent = quote.text;
+    // set quote, hide loader
+    complete();
+}
+
+// Get Quotes from API
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch (apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // catch error here
+    }
+}
+
+// Tweet Quote
+function tweetQuote () {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+// On Load
+getQuotes();
